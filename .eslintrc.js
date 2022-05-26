cdd21908738a979f1216c1782f8d2de776fa92dc
Wellington <wellingtonmachioni@gmail.com>
2022-05-26 19:03:13 -0300

    Configurado Eslint

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+      "react/state-in-constructor": "off",
+      "react/no-did-mount-set-state": "off",
+      "react/no-did-update-set-state": "warn",
+    }
+}
